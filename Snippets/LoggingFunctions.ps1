524220b7fbd63500de2eaafe6d8f2d983768a1b9
agffthwork <agriffith@karmak.com>
2016-08-22 15:22:22 -0500

    Adding a folder for snippets and functions
    Adding a folder for snippets and functions

---
+++ Snippets/LoggingFunctions.ps1
@@ -0,0 +1,11 @@
+﻿Function Log-Line ($message)
+{
+    $message | Out-File $log -Append
+}
+
+Function Log-Error ($message)
+{
+    Log-Line "***ERROR***" 
+    Log-Line $message 
+    Log-Line "***********" 
+}
