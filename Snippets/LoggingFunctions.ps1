63b0285d305b42d6d7295f27cb38b2e59a8b9487
agffthwork <agriffith@karmak.com>
2016-08-22 15:26:16 -0500

    Use Comments
    Added comments to include reminders of setting up the log variable and
    making sure you have the log-line function when using the log-error
    function

--- Snippets/LoggingFunctions.ps1
+++ Snippets/LoggingFunctions.ps1
@@ -1,10 +1,12 @@
 ﻿Function Log-Line ($message)
 {
+    ## Make sure you define the log file variable in the Main function
     $message | Out-File $log -Append
 }
 
 Function Log-Error ($message)
 {
+    ## This function is dependent on the Log-Line function
     Log-Line "***ERROR***" 
     Log-Line $message 
     Log-Line "***********" 
