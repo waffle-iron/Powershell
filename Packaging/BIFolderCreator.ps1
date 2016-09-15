bd2ff5111631f876f31f4bca3a2430a3edca8825
agffthwork <agriffith@karmak.com>
2016-09-15 14:48:16 -0500

    Updated to new standards

--- Packaging/BIFolderCreator.ps1
+++ Packaging/BIFolderCreator.ps1
@@ -16,120 +16,136 @@ ToDo:
 Function Main
 {
 
-## log file creation
-$folder = Get-Location
-$Global:log = "$folder\log.txt"
-$date = Get-Date
-"Log started - $date" | Out-File $log
+    ## log file creation
+    $folder = Get-Location
+    $Global:log = "$folder\log.txt"
+    $date = Get-Date
+    "Log started - $date" | Out-File $log
+    
+    ## define the build branch
+    $count = 0
+    $choices = "PM_3_Current", "PM_3_ServicePack"
+    Do{
+    cls
+    If ($count -gt 0) {Write-Host "Please select a valid option. Failed attempts:" $count}
+        $branch = Read-Host -Prompt "
+        1- Current
+        2- Service Pack
+        Which branch are you releasing out of?"
+        If ($branch -eq '1') {$branch = $choices[0]}
+        If ($branch -eq '2') {$branch = $choices[1]}
+        If ($branch -eq 'exit') {Exit}
+    Else {$count++}
+    } until ($choices -contains $branch)
 
-## define the build branch
-$count = 0
-$choices = "PM_3_Current", "PM_3_ServicePack"
-Do{
-cls
-If ($count -gt 0) {Write-Host "Please select a valid option. Failed attempts:" $count}
-    $branch = Read-Host -Prompt "
-    1- Current
-    2- Service Pack
-    Which branch are you releasing out of?"
-    If ($branch -eq '1') {$branch = $choices[0]}
-    If ($branch -eq '2') {$branch = $choices[1]}
-    If ($branch -eq 'exit') {Exit}
-Else {$count++}
-} until ($choices -contains $branch)
+    Log-Line "You have selected branch: $branch"
 
-Log-Line "You have selected branch: $branch"
+    ## define the build branch folder
+    $build = "\\pmdevsql\Builds\$branch\*"
 
-## define the build branch folder
-$build = "\\pmdevsql\Builds\$branch\*"
-
-## define the distribution folder
-$distro = "C:\DistroTest\BI\"
-## $distro = "\\fp_server\Public\Distribution\RPM\BI 3.0 Releases\"
-
-## =================================================================================================================================================================
-
-## define the previous version folder
-Do{
-$oldVersion = Read-Host -Prompt "
-What is the OLD version number?"
-} until ($oldVersion -ne "" )
-
-## define the new version folder
-Do{
-$newVersion = Read-Host -Prompt "
-What is the NEW version number?"
-} until ($newVersion -ne "")
-
-## define the copy from and copy to folders
-$old = "$distro$oldVersion"
-$new = "$distro$newVersion"
+    ## define the distribution folder
+    ## $distro = "C:\DistroTest\BI\"
+    $distro = "\\fp_server\Public\Distribution\RPM\BI 3.0 Releases\"
 
 ## =================================================================================================================================================================
 
-Try {
+    ## define the previous version folder
+    Do{
+    $oldVersion = Read-Host -Prompt "
+    What is the OLD version number?"
+    } until ($oldVersion -ne "" )
 
-    Log-Line "Creating new folder" 
+    ## define the new version folder
+    Do{
+    $newVersion = Read-Host -Prompt "
+    What is the NEW version number?"
+    } until ($newVersion -ne "")
+
+    ## define the copy from and copy to folders
+    $old = "$distro$oldVersion"
+    $new = "$distro$newVersion"
+
+## =================================================================================================================================================================
 
     ## copy everything from the old version to the new version folder
-    Copy-Item $old\ -Destination "$new\" -Force -Recurse -ErrorVariable e
-    Log-Line $e 
+    Try {
+    Log-Line "Creating new folder" 
+    Copy-Item $old\ -Destination "$new\" -Force -Recurse
+    Log-Line "New folder created"
+    }
+    Catch {
+    Error-Exit
+    }
 
-    Log-Line "New folder created" 
+    
+    ## deletes the files from the new upgrade folder
+    Try {
+    Log-Line "Deleting upgrade files"
+    Remove-Item -Path "$new\Upgrade\*"
+    Log-Line "Upgrade folder cleared" 
+    }
+    Catch {
+    Error-Exit
+    }
+
 
     ## update Upgrade folder
-    Copy-Item $build\ -Destination "$new\Upgrade\Application\" -Force -Recurse -ErrorVariable e
-    Log-Line $e 
-
+    Try {
+    Copy-Item $build\ -Destination "$new\Upgrade\Application\" -Force -Recurse
     Log-Line "Build copied out" 
+    }
+    Catch {
+    Error-Exit
+    }
+    
 
     ## delete zip files
+    Try {
     Remove-Item -Include *.zip -Path "$new\" -Recurse
+    Log-Line "Zip files deleted"
+    }
+    Catch {
+    Error-Exit
+    }
 
-    Log-Line "Zip files deleted" 
 
     ## update New Install folder
-    Copy-Item "$new\Upgrade\Application\" -Destination "$new\New Install\Application\" -Force -Recurse -ErrorVariable e
-    Log-Line $e 
-
+    Try {
+    Copy-Item "$new\Upgrade\Application\" -Destination "$new\New Install\Application\" -Force -Recurse
     Log-Line "New Install updated" 
+    }
+    Catch {
+    Error-Exit
+    }
 
-## ================================================================================================================================================================+
-
-    ## zip application folder
-    $za = "C:\DistroTest\2.0.0.0\Upgrade\Application.zip"
-    $sa = "C:\DistroTest\2.0.0.0\Upgrade\Application\"
-
-    Zip-Files $za $sa
-
-    Log-Line "Application folder zipped" 
 
     ## zip process data folder
-    $zp = "C:\DistroTest\2.0.0.0\Upgrade\Application\ProcessData.zip"
-    $sp = "C:\DistroTest\2.0.0.0\Upgrade\Application\ProcessData"
-
+    Try{
+    $zp = "$new\Application\ProcessData.zip"
+    $sp = "$new\Application\ProcessData"
     Zip-Files $zp $sp
-
     Log-Line "Process Data folder zipped" 
+    }
+    Catch {
+    Error-Exit
+    }
+
 
     ## zip web folder
-    $zw = "C:\DistroTest\2.0.0.0\Upgrade\Application\Web.zip"
-    $sw = "C:\DistroTest\2.0.0.0\Upgrade\Application\Web"
-
+    Try {
+    $zw = "$new\Application\Web.zip"
+    $sw = "$new\Application\Web"
     Zip-Files $zw $sw
-
     Log-Line "Web folder zipped" 
+    }
+    Catch {
+    Error-Exit
+    }
+
 
     Get-Date | Out-File $log -Append
-
     Write-Host "=== BI Packaging Complete ==="
 
-}
-Catch {
-
-    Error-Exit
-
-}
 
 }  #  <----- End of Main
 
@@ -166,7 +182,7 @@ fuction Error-Exit
 {
     Log-Line "***ERROR***" 
     Log-Line $_.Exception.Message 
-    Error-Exit 
+    Exit 
 }
 
 
--- Packaging/FRWFolderCreator.ps1
+++ Packaging/FRWFolderCreator.ps1
@@ -15,77 +15,101 @@ ToDo:
 
 Function Main
 {
-## set the log
-$folder = Get-Location
-$Global:log = "$folder\log.txt"
-$date = Get-Date
-"Log started - $date" | Out-File $log
+	## set the log
+	$folder = Get-Location
+	$Global:log = "$folder\log.txt"
+	$date = Get-Date
+	"Log started - $date" | Out-File $log
 
-## define the build branch
-$count = 0
-$choices = "PM_3_Current", "PM_3_ServicePack"
-Do {
-cls
-If ($count -gt 0) {Write-Host "Please select a valid option. Failed attempts:" $count}
-$branch = Read-Host -Prompt "
-    1- Current
-    2- Service Pack
-    Which branch are you releasing out of?"
-    IF ($branch -eq '1') {$branch = $choices[0]}
-    IF ($branch -eq '2') {$branch = $choices[1]}
-    IF ($branch -eq 'exit') {Exit}
-Else {$count++}
-} until ($choices -contains $branch)
+	## define the build branch
+	$count = 0
+	$choices = "PM_3_Current", "PM_3_ServicePack"
+	Do {
+	cls
+	If ($count -gt 0) {Write-Host "Please select a valid option. Failed attempts:" $count}
+	$branch = Read-Host -Prompt "
+		1- Current
+		2- Service Pack
+		Which branch are you releasing out of?"
+		IF ($branch -eq '1') {$branch = $choices[0]}
+		IF ($branch -eq '2') {$branch = $choices[1]}
+		IF ($branch -eq 'exit') {Exit}
+	Else {$count++}
+	} until ($choices -contains $branch)
 
-Write-Host "You have selected branch: "$branch
+	Write-Host "You have selected branch: "$branch
 
-## define the build branch folder
-$build = "\\pmdevsql\Builds\$branch\Cumulative\Analytics\FusionSelfService\"
+	## define the build branch folder
+	$build = "\\pmdevsql\Builds\$branch\Cumulative\Analytics\FusionSelfService\"
 
-## define the distribution folder
-$distro = "C:\DistroTest\FRW\"
-## $distro = "\\fp_server\Public\Distribution\FusionReportWriter\"
+	## define the distribution folder
+	## $distro = "C:\DistroTest\FRW\"
+	$distro = "\\fp_server\Public\Distribution\FusionReportWriter\"
 
 
-## define the previous version folder
-Do{
-    $oldVersion = Read-Host -Prompt "
-    What is the OLD version number?"
-} until ($oldVersion -ne "" )
+	## define the previous version folder
+	Do{
+		$oldVersion = Read-Host -Prompt "
+		What is the OLD version number?"
+	} until ($oldVersion -ne "" )
 
-## define the new version folder
-Do{
-    $newVersion = Read-Host -Prompt "
-    What is the NEW version number?"
-} until ($newVersion -ne "")
+	## define the new version folder
+	Do{
+		$newVersion = Read-Host -Prompt "
+		What is the NEW version number?"
+	} until ($newVersion -ne "")
 
     ## create the new version folder
+    Try {
     new-item $distro -name "$newVersion" -type directory
+    }
+    Catch {
+    Error-Exit
+    }
 
     ## define the copy from and copy to folders
     $old = "$distro$oldVersion\"
     $new = "$distro$newVersion\"
     
     ## create the new folder structure
+    Try {
     Log-Line "Creating folders"
     new-item -path $new -name "Complete Build" -type directory
     new-item -path $new -name "Cumulative" -type directory
     new-item -path $new -name "Service Pack" -type directory
     Log-Line "Folders created"
+    }
+    Catch {
+    Error-Exit
+    }
 
     ## copy the complete build folder to the new version folder
+    Try {
     Log-Line "Copying Complete Build"
     Copy-Item "$old\Complete Build" -Destination "$new" -Force -Recurse 
     Log-Line "Finished copying Complete Build"
+    }
+    Catch {
+    Error-Exit
+    }
 
+    Try {
     Log-Line "Copying Cumulative"
     Copy-Item "$old\Cumulative" -Destination "$new" -Force -Recurse 
     Log-Line "Finished copying Cumulative"
+    }
+    Catch {
+    Error-Exit
+    }
 
+    Try {
     Log-Line "Copying Service Pack"
     Copy-Item "$build\*" -Destination "$new\Service Pack" -Force -Recurse 
     Log-Line "Finished copying Service Pack"
-
+    }
+    Catch {
+    Error-Exit
+    }
 }
 
 
